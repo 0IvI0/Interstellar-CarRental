0f1290abdf02e72e7061e75b0ea5e3ab38a22755
Ivett Kiss <iv.kiss@yahoo.com>
2020-10-24 00:12:48 +0200

    fixed typo

--- carrental_backend/src/main/java/com/interstellarcarrental/carrental/controllers/CarController.java
+++ carrental_backend/src/main/java/com/interstellarcarrental/carrental/controllers/CarController.java
@@ -6,12 +6,14 @@ import com.interstellarcarrental.carrental.dto.CarDTO;
 import com.interstellarcarrental.carrental.services.CarService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class CarController {
     
