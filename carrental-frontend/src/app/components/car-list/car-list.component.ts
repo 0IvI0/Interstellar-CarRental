1fa80cd3146ca4028161fe4b821bd615fb1b4b19
Ivett Kiss <iv.kiss@yahoo.com>
2020-10-26 19:14:34 +0100

    Created Car-list component

---
+++ carrental-frontend/src/app/components/car-list/car-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Car } from 'src/app/models/Car';
+import { CarService } from 'src/app/services/car.service';
+
+@Component({
+  selector: 'app-car-list',
+  templateUrl: './car-list.component.html',
+  styleUrls: ['./car-list.component.css']
+})
+export class CarListComponent implements OnInit {
+
+  carList : Car[];
+
+  constructor(private carService:CarService) {
+   }
+
+  ngOnInit(): void {
+    this.carService.getCarList().subscribe(carList => {
+      this.carList = carList;
+    });
+  }
+
+}
