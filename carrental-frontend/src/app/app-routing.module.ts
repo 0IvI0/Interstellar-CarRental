2017e3b4432d676a00d2c75b26520f1ed501fe38
Ivett Kiss <iv.kiss@yahoo.com>
2020-11-09 17:17:46 +0100

    Added routes to app-routing.module.ts

--- carrental-frontend/src/app/app-routing.module.ts
+++ carrental-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarListComponent } from './components/car-list/car-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'carlist', component: CarListComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
