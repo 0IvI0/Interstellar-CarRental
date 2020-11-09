6ed74d9846c3b1c4ba8b5bb70b7702910c4a16f9
Ivett Kiss <iv.kiss@yahoo.com>
2020-11-09 22:16:15 +0100

    Added form controls

--- carrental-frontend/src/app/components/login/login.component.ts
+++ carrental-frontend/src/app/components/login/login.component.ts
@@ -1,28 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-/* import { HttpClient } from '@angular/common/http'; */
-import { Router } from '@angular/router';
-/* import { AppService } from '../../app.service'; */
+import { Component, Injectable, OnInit } from '@angular/core';
+import { AppService } from '../../app.service';
+import { Customer } from 'src/app/models/Customer';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+@Injectable()
 export class LoginComponent implements OnInit {
 
- /*  credentials = {username: '', password: ''}; */
-
-  constructor(/* private app: AppService, private http: HttpClient, */ private router: Router) {
-  }
-
-  login() {
-/*     this.app.authenticate(this.credentials, () => {
-        this.router.navigateByUrl('/');
+    formLogin = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
     });
-    return false; */
+
+    sessionUser: Customer | null = null;
+
+    private loginCredentials = {username: '', password: ''};
+
+    constructor(private app: AppService) {
   }
 
-  ngOnInit(): void {
+
+  onSubmit() {
+
+    if (this.formLogin.invalid) {
+      return;
+    }
+
+    this.loginCredentials = this.formLogin.value;
+    console.log(this.loginCredentials);
+
+    this.app.login(this.loginCredentials.username, this.loginCredentials.password);
   }
 
+  logout() {
+      this.app.logout();
+  }
+
+
+  ngOnInit() {
+    this.app.getSessionUser().subscribe(u => this.sessionUser = u);
+  }
 }
