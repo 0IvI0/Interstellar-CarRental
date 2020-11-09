3d17d7277537a96aeab75db8e458842256998040
Ivett Kiss <iv.kiss@yahoo.com>
2020-11-09 17:23:22 +0100

    Configured loing, logout and register methods

--- carrental-frontend/src/app/app.service.ts
+++ carrental-frontend/src/app/app.service.ts
@@ -1,28 +1,62 @@
 import { Injectable } from '@angular/core';
-/* import { HttpClient, HttpHeaders } from '@angular/common/http'; */
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Customer } from './models/Customer';
+import { Router } from '@angular/router';
 
-@Injectable()
+
+@Injectable({
+    providedIn: 'root'
+})
 export class AppService {
 
-/*   authenticated = false;
+    sessionUser = new BehaviorSubject<Customer|null>(null);
 
-  constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private router: Router) {
+        this.http.get<Customer>('/api/sessionUser').subscribe(u => this.sessionUser.next(u));
+    }
+
+    public getSessionUser(): Observable<Customer|null> {
+        return this.sessionUser;
+      }
+
+
+    public login(username: string, password: string) {
+    this.http.get<Customer>('/api/sessionUser', {
+      headers: {
+        authorization: 'Basic ' + btoa(username + ':' + password)
+      }
+    }).subscribe(
+      u => {
+        this.sessionUser.next(u);
+        if (u != null) {
+          // wir sind eingeloggt
+          if (u.userRole == "EMPLOYEE") {
+            console.log('Hi, I am an employee.');
+            //this.router.navigate(['employee']);
+          } else {
+            console.log('I am a customer.');
+            //this.router.navigate(['customer']);
+          }
+        }
+      },
+      () => this.sessionUser.next(null),
+    );
+  }  
+
+
+  public logout() {
+    this.http.post('/api/logout', null).subscribe(
+      () => {
+        this.router.navigate(['/']);
+        this.sessionUser.next(null);
+      }
+    );
   }
 
-  authenticate(credentials, callback) {
 
-        const headers = new HttpHeaders(credentials ? {
-            authorization : 'Basic ' + btoa(credentials.username + ':' + credentials.password)
-        } : {});
-
-        this.http.get('user', {headers: headers}).subscribe(response => {
-            if (response['name']) {
-                this.authenticated = true;
-            } else {
-                this.authenticated = false;
-            }
-            return callback && callback();
-        });
-    } */
+  public register(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>('/api/register', customer);
+  }
 
 }
