d3da4b2bbc1ca62269406285c907c5496a62eb3e
Ivett Kiss <iv.kiss@yahoo.com>
2020-11-09 22:20:28 +0100

    Created Employee model

---
+++ carrental-frontend/src/app/models/Employee.ts
@@ -0,0 +1,15 @@
+export interface Employee {
+
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    userRole: string;
+    //birthDate: number; //LocalDate
+    emailAddress: string;
+    phoneNumber: string;
+    address: string;
+    creditCardNumber: string;
+    //List<Invoice> invoiceList:
+
+}
