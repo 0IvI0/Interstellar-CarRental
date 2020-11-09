f8e29ca56b70af2b8fcb6b8538c3d1fef21dd53d
Ivett Kiss <iv.kiss@yahoo.com>
2020-11-09 22:57:19 +0100

    removed annotation

--- carrental_backend/src/main/java/com/interstellarcarrental/carrental/CarrentalApplication.java
+++ carrental_backend/src/main/java/com/interstellarcarrental/carrental/CarrentalApplication.java
@@ -3,6 +3,7 @@ package com.interstellarcarrental.carrental;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CarrentalApplication {
 
