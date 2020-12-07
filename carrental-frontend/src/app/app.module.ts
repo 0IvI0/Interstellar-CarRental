428336a9a03c5ac8c14e61f8849a9d1c423c5cc8
Ivett Kiss <iv.kiss@yahoo.com>
2020-12-07 13:32:06 +0100

    Updated the app module with the new components

--- carrental-frontend/src/app/app.module.ts
+++ carrental-frontend/src/app/app.module.ts
@@ -5,27 +5,35 @@ import { ReactiveFormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SearchFilterPipe } from './pipes/search-filter.pipe';
+import { BooleanDetailsPipe } from './pipes/booleanDetails.pipe';
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { CarComponent } from './components/car/car.component';
 import { CarListComponent } from './components/car-list/car-list.component';
 import { RegisterComponent } from './components/register/register.component';
 import { SessionUserComponent } from './components/session-user/session-user.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { CarDetailsComponent } from './components/car-details/car-details.component';
+import { RentConfirmationComponent } from './components/rent-confirmation/rent-confirmation.component';
+import { OrderingPageComponent } from './components/ordering-page/ordering-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SearchFilterPipe,
+    BooleanDetailsPipe,
     HomeComponent,
     LoginComponent,
-    CarComponent,
     CarListComponent,
     RegisterComponent,
     SessionUserComponent,
     LogoComponent,
+    CarDetailsComponent,
+    RentConfirmationComponent,
+    OrderingPageComponent,
   ],
   imports: [
     BrowserModule,
