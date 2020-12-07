fea98a144bc92bcb80c49d97409f79db2cddfcd4
Ivett Kiss <iv.kiss@yahoo.com>
2020-12-07 13:29:18 +0100

    Added more paths

--- carrental-frontend/src/app/app-routing.module.ts
+++ carrental-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarDetailsComponent } from './components/car-details/car-details.component';
 import { CarListComponent } from './components/car-list/car-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { OrderingPageComponent } from './components/ordering-page/ordering-page.component';
+import { RentConfirmationComponent } from './components/rent-confirmation/rent-confirmation.component';
 
 
 const routes: Routes = [
@@ -12,6 +15,9 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'carlist', component: CarListComponent },
+  { path: 'cardetails/:vehicleID', component: CarDetailsComponent },
+  { path: 'order-processing', component: OrderingPageComponent },
+  { path: 'order-confirmation', component: RentConfirmationComponent },
 ];
 
 
