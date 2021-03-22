27134e7aca1057a6491e6381eb209e8697971585
Ivett Kiss <iv.kiss@yahoo.com>
2021-03-22 17:22:25 +0100

    Changed url path in annotation

--- carrental_backend/src/main/java/com/interstellarcarrental/carrental/controllers/EmployeeController.java
+++ carrental_backend/src/main/java/com/interstellarcarrental/carrental/controllers/EmployeeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController("/api")
+@RestController("/api/employee")
 public class EmployeeController {
 
     @Autowired
@@ -22,7 +22,7 @@ public class EmployeeController {
     
 //POST MAPPING
 
-    @PostMapping("/company-admin/createEmployee")
+    @PostMapping("/admin/createEmployee")
     public String saveEmployee(EmployeeDTO employeeDto) {
         employeeService.saveEmployee(employeeDto);
         return "Registration of new employee successful!";
@@ -33,32 +33,32 @@ public class EmployeeController {
 
 //For manager
 
-   @GetMapping("/employee/mngr/listEmployees")
+   @GetMapping("/mngr/listEmployees")
     public List<EmployeeDTO> listEmployees() {
         return employeeService.getEmployees();
     }
 
-    @GetMapping("/employee/mngr/listEmployee/{username}")
+    @GetMapping("mngr/listEmployee/{username}")
     public EmployeeDTO getEmployeeOfUsername(@PathVariable String username) {
         return employeeService.getEmployeeByUsername(username);
     }
 
-    @GetMapping("/employee/mngr/listEmployee/{lastName}")
+    @GetMapping("mngr/listEmployee/{lastName}")
     public List<EmployeeDTO> listEmployeesMatchingLastName(@PathVariable String lastName) {
         return employeeService.getEmployeeByLastNameIgnoreCaseOrderByFirstNameAsc(lastName);
     }
 
-    @GetMapping("/employee/mngr/listEmployee/{firstName}")
+    @GetMapping("/mngr/listEmployee/{firstName}")
     public List<EmployeeDTO> listEmployeesMatchingFirstName(@PathVariable String firstName) {
         return employeeService.getEmployeeByFirstName(firstName);
     }
 
-    @GetMapping("/employee/mngr/listEmployee/{birthDate}")
+    @GetMapping("/mngr/listEmployee/{birthDate}")
     public List<EmployeeDTO> listEmployeesByBirthDate(@PathVariable LocalDate birthdate) {
         return employeeService.getEmployeeByBirthDate(birthdate);
     }
 
-    @GetMapping("/employee/mngr/listEmployee/{emailAddress}")
+    @GetMapping("/mngr/listEmployee/{emailAddress}")
     public EmployeeDTO getEmployeeMatchingEmailAddress(@PathVariable String emailAddress) {
         return employeeService.getEmployeeByEmailAddress(emailAddress);
     }
