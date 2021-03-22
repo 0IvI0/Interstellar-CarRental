87d0e29906686124d8cef9ddddb9ae505ef96881
Ivett Kiss <iv.kiss@yahoo.com>
2021-03-22 14:55:39 +0100

    created Role repository

---
+++ carrental_backend/src/main/java/com/interstellarcarrental/carrental/repositories/RoleRepository.java
@@ -0,0 +1,16 @@
+package com.interstellarcarrental.carrental.repositories;
+
+import java.util.List;
+
+import com.interstellarcarrental.carrental.models.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    
+    Role findByRoleName(String roleName);
+    Role findByRoleDescription(String roleDescription);
+
+    List<Role> findRoleByUserId(long userId);
+    List<Role> findRoleByUsername(String username);
+}
