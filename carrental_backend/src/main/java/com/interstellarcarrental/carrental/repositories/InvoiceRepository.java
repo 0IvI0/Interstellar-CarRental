1ac848d41e6e87dc12d10bfac42e8badff7faffe
Ivett Kiss <iv.kiss@yahoo.com>
2021-03-22 14:38:04 +0100

    Changed invoiceOwner from Customer type to User

--- carrental_backend/src/main/java/com/interstellarcarrental/carrental/repositories/InvoiceRepository.java
+++ carrental_backend/src/main/java/com/interstellarcarrental/carrental/repositories/InvoiceRepository.java
@@ -2,15 +2,15 @@ package com.interstellarcarrental.carrental.repositories;
 
 import java.util.List;
 
-import com.interstellarcarrental.carrental.models.Customer;
 import com.interstellarcarrental.carrental.models.Invoice;
+import com.interstellarcarrental.carrental.models.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
     
-    List<Invoice> findByInvoiceOwner(Customer invoiceOwner);
+    List<Invoice> findByInvoiceOwner(User invoiceOwner);
     List<Invoice> findByPaidInvoice(Boolean paidInvoice);
     Invoice findByInvoiceNumber(Integer invoiceNumber);
 }
