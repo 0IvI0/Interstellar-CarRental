1190d8ef483caf67de68b172d0788a0ff386e43c
Ivett Kiss <iv.kiss@yahoo.com>
2021-03-22 13:48:29 +0100

    Added UserDTO

---
+++ carrental_backend/src/main/java/com/interstellarcarrental/carrental/dto/UserDTO.java
@@ -0,0 +1,25 @@
+package com.interstellarcarrental.carrental.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.interstellarcarrental.carrental.models.Invoice;
+
+import lombok.Data;
+
+
+@Data
+public class UserDTO {
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String emailAddress;
+    private String phoneNumber;
+    private String address;
+    private String creditCardNumber;
+
+    private List<Invoice> invoiceList;
+}
